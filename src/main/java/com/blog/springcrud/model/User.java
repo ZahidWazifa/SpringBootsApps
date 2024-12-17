407edad2526588cf798f195ab83cf1d00bf90d4c
Zahid Wazifa <acerzifa@gmail.com>
2024-12-17 18:31:11 +0700

    feat: finish projects

--- src/main/java/com/blog/springcrud/model/User.java
+++ src/main/java/com/blog/springcrud/model/User.java
@@ -8,53 +8,44 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    @Column(name="name")
+    @Column(name = "name")
     private String name;
     @Column(name = "city")
     private String city;
     @Column(name = "email")
     private String email;
-
-    public User(){
+    public User() {
         super();
     }
-
     public User(long id, String name, String city, String email) {
+        super();
         this.id = id;
         this.name = name;
-        city = city;
-        email = email;
+        this.city = city;
+        this.email = email;
     }
-
     public long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getCity() {
         return city;
     }
-
     public void setCity(String city) {
-        city = city;
+        this.city = city;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
-        email = email;
+        this.email = email;
     }
 }
